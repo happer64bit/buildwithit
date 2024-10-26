aeb13c3d2f3a3093edb9207ce248c2e8365149f6
happer64bit <happer64bit@gmail.com>
2024-10-26 20:02:55 +0630

    vercel adapter

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # BuildWithIt
 
+BuildWithIt is a platform designed to showcase websites built with modern frameworks. Contributors can add their own websites, making it a valuable resource for developers and users looking for inspiration and examples of different technologies.
+
 ![Screenshot Of Website](.github/resources/screenshot-1.png)
+
+## Contributing
+
+If you want to add your website in this, see [CONTRIBUTING.md](CONTRIBUTING.md)
--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import { mdsvex } from "mdsvex";
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-node'
+import vercelAdapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: import.meta.env.PROD ? vercelAdapter (): adapter()
 	},
 
     extensions: [".svelte", ".svx"]
