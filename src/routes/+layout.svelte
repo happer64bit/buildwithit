b75a813fab17864b06be43bd7dcabd8ba9d1ffe8
happer64bit <happer64bit@gmail.com>
2024-10-27 22:00:29 +0630

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 	import '../app.css';
 	import '@fontsource-variable/instrument-sans';
 	import Navbar from '../components/Navbar.svelte';
+	import Analytics from '../components/Analytics.svelte';
 
 	let { children } = $props();
 </script>
 
+<Analytics />
 <Navbar />
 {@render children()}
